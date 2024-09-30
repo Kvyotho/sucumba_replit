3262922023610eb8f394061342987cf919661f5a
Kvyotho <159950199+Kvyotho@users.noreply.github.com>
2024-09-30 08:47:27 -0300

    Add files via upload

---
+++ desafio_3.py
@@ -0,0 +1,13 @@
+def isPalindrome(number):
+
+    i = 0
+    for i in range(len(number)):
+
+
+
+    #return True
+
+    
+print(isPalindrome(10)) #False
+print(isPalindrome(101)) #True
+print(isPalindrome(-101)) #False
